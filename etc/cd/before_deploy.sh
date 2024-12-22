cde60a424b4512db0fc993b922dac2f4148abbcf
Shigure Kurosaki <shigure@hqsy.net>
2024-12-23 04:22:55 +0800

    revert: non-standard format
    This reverts commit 204bb2619e0198aaac9a625a79d66bbee50c05f1.

--- etc/cd/before_deploy.sh
+++ etc/cd/before_deploy.sh
@@ -150,7 +150,7 @@ Conflicts: $conflictname
 Description: CLI tool for generating RSS feeds.
 EOF
 
-    fakeroot dpkg-deb -Zxz --build "$tempdir" "${dpkgname}_${version/-canary./~canary}-1_${architecture}.deb"
+    fakeroot dpkg-deb -Zxz --build "$tempdir" "${dpkgname}_${version}_${architecture}.deb"
 }
 
 
